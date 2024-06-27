58d62c3da500d1a33b514906be311e68bd9bd7a4
Amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-06-27 15:27:59 +0330

    update to part 13

--- account/views.py
+++ account/views.py
@@ -4,11 +4,18 @@ from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class RegisterView(View):
     form_class = RegisterForm
     template_name = 'account/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.warning(request, 'You are already logged in', 'warning')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
         form = self.form_class()
@@ -28,6 +35,12 @@ class UserLoginView(View):
     form_class = LoginForm
     template_name = 'account/login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.warning(request, 'You are already logged in', 'warning')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         form = self.form_class
         return render(request, self.template_name, {'form': form})
@@ -46,7 +59,7 @@ class UserLoginView(View):
         return render(request, self.template_name, {'form': form})
 
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
         messages.success(request, 'You are now logged out', 'success')
