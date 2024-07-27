443dac665889337af6bb544fd481a456346d8558
AmirSalar <hosseiniseyedamirsalar@gmail.com>
2024-07-27 19:27:46 +0330

    Update settings.py(edit reset password's backend)

--- social_network/settings.py
+++ social_network/settings.py
@@ -135,8 +135,8 @@ AUTHENTICATION_BACKENDS = [
 # Google account
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'assassincreed3445@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'rooxxatjcexzzzad'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'PK website'
